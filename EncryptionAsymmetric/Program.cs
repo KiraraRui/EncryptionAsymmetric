9f923432defda5561c6d1eed8f06bfcda6005be1
Kira0698 <kira0698@zbc.dk>
2021-04-22 21:11:59 +0200

    initial commit

---
+++ EncryptionAsymmetric/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionAsymmetric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ama do it ina  bit for now a need a break like just 2 hours pleaze.
+        }
+    }
+}
